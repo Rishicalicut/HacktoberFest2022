ba2a19bc6284c99fe7d20be2e499009d65a0e07b
Aliya A <72246025+aliyakalam@users.noreply.github.com>
2022-10-30 19:31:34 +0530

    Create metodparameter.java

---
+++ metodparameter.java
@@ -0,0 +1,11 @@
+public class metodparameter {
+	static void printmessage(String name) {
+		System.out.println("hello "+name);
+	}
+	
+	public static void main(String[] args) {
+		printmessage("aliya");
+		printmessage("rishi");
+	}
+
+}
